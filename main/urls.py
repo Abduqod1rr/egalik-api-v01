4a998222f11ac9acabe946373d2e8dc3e339853e
abduqodir <ttheonepersonn@gmail.com>
2025-12-02 14:21:12 +0000

    my products

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("add/", views.AddProduct.as_view(), name="addproduct"),
     path("/", views.Home.as_view(), name="home"),
+    path("mening_elonlarim/", views.MyProducts.as_view(), name="mening_elonlarim")
 ]
