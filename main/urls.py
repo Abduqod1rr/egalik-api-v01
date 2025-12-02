44e2459f5d63d0ed077d8413ec2d6c480050e9d8
abduqodir <ttheonepersonn@gmail.com>
2025-12-02 14:36:28 +0000

    full crud

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("add/", views.AddProduct.as_view(), name="addproduct"),
     path("/", views.Home.as_view(), name="home"),
     path("mening_elonlarim/", views.MyProducts.as_view(), name="mening_elonlarim"),
-    path("CRUD/<int:pk>", views.productCrud.as_view(), name="CRUD")
+    path("CRUD/<int:pk>/", views.productCrud.as_view(), name="CRUD")
 ]
