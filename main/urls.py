709f34c2e5ff2a696f7bccb2de08348ad7213340
abduqodir <ttheonepersonn@gmail.com>
2025-12-03 12:05:58 +0000

    allows host

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("add/", views.AddProduct.as_view(), name="addproduct"),
-    path("home/", views.Home.as_view(), name="home"),
+    path("", views.Home.as_view(), name="home"),
     path("mening_elonlarim/", views.MyProducts.as_view(), name="mening_elonlarim"),
     path("CRUD/<int:pk>/", views.productCrud.as_view(), name="CRUD")
 ]
