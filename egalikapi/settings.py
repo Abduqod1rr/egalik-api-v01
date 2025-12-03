35e7676d6c1a55a7ff4102824e025ac1914b8a32
abduqodir <ttheonepersonn@gmail.com>
2025-12-03 13:16:06 +0000

    dj filter

--- egalikapi/settings.py
+++ egalikapi/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
     'main',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.registerUser.as_view(), name="register"),
-    path("login/", views.loginUser.as_view(), name="login"),
+    path("register/", views.registerUser.as_view(), name="register"),# bulardan ols=din auth/ bor
+    path("login/", views.loginUser.as_view(), name="login"), 
     path("logout/", views.logoutUser.as_view(), name="logout")
     
 ]
