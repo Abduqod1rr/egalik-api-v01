97e4cc4080b534849f4fc9037a624eb2c6ecd1a9
abduqodir <ttheonepersonn@gmail.com>
2025-12-04 11:08:48 +0000

    rasmlar korinadigan

--- egalikapi/urls.py
+++ egalikapi/urls.py
@@ -24,4 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('auth/',include('users.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
