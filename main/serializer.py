efabed51dc350231f13969f5e922eb0c6bc0002f
abduqodir <ttheonepersonn@gmail.com>
2025-12-04 10:51:58 +0000

    id owner

--- main/serializer.py
+++ main/serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Products
 
 class ProductSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Products
         fields = ['id','owner','holat','title','kategoriya','tavsilot','narx','rasm','number','telegram']
