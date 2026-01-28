cae91fe6c9eca840038adc01471da92e7074e998
abduqodir <ttheonepersonn@gmail.com>
2026-01-28 17:25:22 +0500

    healthz/

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import healthz
 
 urlpatterns = [
-    path("healthz",healthz),
+    path("healthz/",healthz),
     path("add/", views.AddProduct.as_view(), name="addproduct"),
     path("", views.Home.as_view(), name="home"),
     path("mening_elonlarim/", views.MyProducts.as_view(), name="mening_elonlarim"),
